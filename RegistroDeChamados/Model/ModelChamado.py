e3ae285717c99e0277b74f34ddb55b4737039a0e
Diego Alves de Assis <M206255@eletropaulo.com.br>
2019-09-27 16:01:10 -0300

    melhorias diversas

--- RegistroDeChamados/Model/ModelChamado.py
+++ RegistroDeChamados/Model/ModelChamado.py
@@ -11,10 +11,27 @@ sqla = SQLAlchemy()
 class Chamado(sqla.Model):
     __tablename__ = 'tblChamado'
     numeroChamado = sqla.Column(sqla.Integer, primary_key = True)
+    solicitanteNome = sqla.Column(sqla.String(30), nullable = False)
+    solicitanteTelefone = sqla.Column(sqla.String(14), nullable = False)
+    solicitanteLocalidade = sqla.Column(sqla.String(30), nullable = False)
+    #TODO: Incluir dados de log para possivel rastreio e auditoria
     tipo =  sqla.relationship('TipoChamado', backref = 'tblChamado', lazy = True)
     sistema = sqla.relationship('Sistema', backref='tblChamado', lazy = True)
-    descricaoProblema = sqla.Column(sqla.String(100), nullable = False)
     origemReclamacao = sqla.relationship('OrigemReclamacao', backref = 'tblChamado', lazy = True)
+    tempoLentidao = sqla.Column(sqla.Integer, nullable = False)
+    descricaoProblema = sqla.Column(sqla.String(100), nullable = False)
+    processoConclui = sqla.Column(sqla.String(20), nullable = False)
+    problemaLojas = sqla.Column(sqla.String(20), nullable = False)
+    problemaEmAllWSLojas = sqla.Column(sqla.String(20), nullable = False)
+    problemaEnel = sqla.Column(sqla.String(20), nullable = False)
+    problemaEmAllWSEnel = sqla.Column(sqla.String(20), nullable = False)
+    problemaCCenter = sqla.Column(sqla.String(50), nullable = False)
+    chamadoAberto = sqla.Column(sqla.String(50), nullable = False)
+    nmrChamadoAberto = sqla.Column(sqla.String(20), nullable = True)
+    processoConclui = sqla.Column(sqla.String(20), nullable = False)
+    contatoNome = sqla.Column(sqla.String(30), nullable = False)
+    contatoTelefone = sqla.Column(sqla.String(14), nullable = False)
+    contatoLocalidade = sqla.Column(sqla.String(30), nullable = False)
     #TODO: criar o campo e tabela de status do chamado, alem de complementar as informações com os dados do solicitante
     
 class TipoChamado(sqla.Model):
@@ -34,3 +51,4 @@ class OrigemReclamacao (sqla.Model):
     id = sqla.Column(sqla.Integer, primary_key = True)
     area = sqla.Column(sqla.String(30), nullable = False)
     fkNumeroChamado = sqla.Column(sqla.Integer, sqla.ForeignKey('tblChamado.numeroChamado'), nullable = False)
+    
